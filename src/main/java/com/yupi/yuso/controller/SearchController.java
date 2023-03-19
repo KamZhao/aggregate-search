17cb0d878fec6fd32bb09f059e8786f3e41ba77b
liyupi <592789970@qq.com>
2023-03-19 23:29:58 +0800

    add 添加搜索门面 add 添加多数据源，使用设计模式进行优化

--- src/main/java/com/yupi/yuso/controller/SearchController.java
+++ src/main/java/com/yupi/yuso/controller/SearchController.java
@@ -1,19 +1,10 @@
 package com.yupi.yuso.controller;
 
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.yupi.yuso.common.BaseResponse;
-import com.yupi.yuso.common.ErrorCode;
 import com.yupi.yuso.common.ResultUtils;
-import com.yupi.yuso.exception.BusinessException;
-import com.yupi.yuso.exception.ThrowUtils;
-import com.yupi.yuso.model.dto.picture.PictureQueryRequest;
-import com.yupi.yuso.model.dto.post.PostQueryRequest;
+import com.yupi.yuso.manager.SearchFacade;
 import com.yupi.yuso.model.dto.search.SearchRequest;
-import com.yupi.yuso.model.dto.user.UserQueryRequest;
-import com.yupi.yuso.model.entity.Picture;
-import com.yupi.yuso.model.vo.PostVO;
 import com.yupi.yuso.model.vo.SearchVO;
-import com.yupi.yuso.model.vo.UserVO;
 import com.yupi.yuso.service.PictureService;
 import com.yupi.yuso.service.PostService;
 import com.yupi.yuso.service.UserService;
@@ -25,8 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
 
 /**
  * 图片接口
@@ -48,43 +37,12 @@ public class SearchController {
     @Resource
     private PictureService pictureService;
 
+    @Resource
+    private SearchFacade searchFacade;
+
     @PostMapping("/all")
     public BaseResponse<SearchVO> searchAll(@RequestBody SearchRequest searchRequest, HttpServletRequest request) {
-        String searchText = searchRequest.getSearchText();
-
-        CompletableFuture<Page<UserVO>> userTask = CompletableFuture.supplyAsync(() -> {
-            UserQueryRequest userQueryRequest = new UserQueryRequest();
-            userQueryRequest.setUserName(searchText);
-            Page<UserVO> userVOPage = userService.listUserVOByPage(userQueryRequest);
-            return userVOPage;
-        });
-
-        CompletableFuture<Page<PostVO>> postTask = CompletableFuture.supplyAsync(() -> {
-            PostQueryRequest postQueryRequest = new PostQueryRequest();
-            postQueryRequest.setSearchText(searchText);
-            Page<PostVO> postVOPage = postService.listPostVOByPage(postQueryRequest, request);
-            return postVOPage;
-        });
-
-        CompletableFuture<Page<Picture>> pictureTask = CompletableFuture.supplyAsync(() -> {
-            Page<Picture> picturePage = pictureService.searchPicture(searchText, 1, 10);
-            return picturePage;
-        });
-
-        CompletableFuture.allOf(userTask, postTask, pictureTask).join();
-        try {
-            Page<UserVO> userVOPage = userTask.get();
-            Page<PostVO> postVOPage = postTask.get();
-            Page<Picture> picturePage = pictureTask.get();
-            SearchVO searchVO = new SearchVO();
-            searchVO.setUserList(userVOPage.getRecords());
-            searchVO.setPostList(postVOPage.getRecords());
-            searchVO.setPictureList(picturePage.getRecords());
-            return ResultUtils.success(searchVO);
-        } catch (Exception e) {
-            log.error("查询异常", e);
-            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "查询异常");
-        }
+        return ResultUtils.success(searchFacade.searchAll(searchRequest, request));
     }
 
 }
